50f9de29031de0dcb86b3c12c0efcd40e6f787c6
Kim Richardson <krichardson@viewdale.com.au>
2015-11-21 15:12:42 +1000

    Updated Android to support passing customer id through to token server

--- src/android/MyBraintree.java
+++ src/android/MyBraintree.java
@@ -27,34 +27,36 @@ public class MyBraintree extends CordovaPlugin {
     private CallbackContext callbackContext;
 
     private String SERVER_BASE;
+    private String CUSTOMER_ID;
     private String clientToken;
     private String amount;
     private String primaryDescription;
     private String secondaryDescription;
 
     private final String TAG = "com.my.mybraintree";
-    
+
     private Context mContext;
 	private Gson gson = new Gson();
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        Log.v(TAG,"Init CoolPlugin");
-        
+        //Log.v(TAG,"Init CoolPlugin");
+
         mContext = cordova.getActivity().getApplicationContext();
     }
-    
+
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext)
     throws JSONException {
-    	Log.v(TAG,"execute");
+    	//Log.v(TAG,"execute");
         if (action.equals("Pay")) {
-        	Log.v(TAG,"Call Pay");
-        	
+        	//Log.v(TAG,"Call Pay");
+
         	SERVER_BASE = args.getString(0);
-        	amount = args.getString(1);
-        	primaryDescription = args.getString(2);
-        	secondaryDescription = args.getString(3);
-        	
+            CUSTOMER_ID = args.getString(1)
+        	amount = args.getString(2);
+        	primaryDescription = args.getString(3);
+        	secondaryDescription = args.getString(4);
+
             getToken();
 
             this.callbackContext = callbackContext;
@@ -67,7 +69,7 @@ public class MyBraintree extends CordovaPlugin {
         }
         return false;
     }
-    
+
     private void ShowDropIn() {
         Customization customization = new Customization.CustomizationBuilder()
         .primaryDescription(primaryDescription)
@@ -75,23 +77,23 @@ public class MyBraintree extends CordovaPlugin {
         .amount(amount)
         .submitButtonText("Pay")
         .build();
-        
+
         Intent intent = new Intent(mContext, BraintreePaymentActivity.class);
         intent.putExtra(BraintreePaymentActivity.EXTRA_CUSTOMIZATION, customization);
         intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, clientToken);
-        
+
         cordova.setActivityResultCallback (this);
         cordova.getActivity().startActivityForResult(intent, REQUEST_CODE);
     }
-    
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
     	if (requestCode == REQUEST_CODE) {
 	        if (resultCode == BraintreePaymentActivity.RESULT_OK) {
 	        	// using Android SDK v1.
 	            String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
-	            
-                Log.v(TAG,"Payment Method Nonce : %s" + paymentMethodNonce);
+
+                //Log.v(TAG,"Payment Method Nonce : %s" + paymentMethodNonce);
 
 	            RequestParams requestParams = new RequestParams();
 	            requestParams.put("payment_method_nonce", paymentMethodNonce);
@@ -113,12 +115,12 @@ public class MyBraintree extends CordovaPlugin {
                             PluginResult result = new PluginResult(PluginResult.Status.ERROR);
                             callbackContext.sendPluginResult(result);
                         }
-	                		
+
 	                	Log.v(TAG, "Payment result : " + content);
-	
+
 	                }
 	            });
-	        	
+
 	        	// using Android SDK v2.
 //	            PaymentMethod paymentMethod = data.getParcelableExtra(
 //	                    BraintreePaymentActivity.EXTRA_PAYMENT_METHOD
@@ -126,14 +128,14 @@ public class MyBraintree extends CordovaPlugin {
 //                String nonce = paymentMethod.getNonce();
 //            	Log.v(TAG, "Nonce : " + nonce);
 //
-//                
+//
 //                // Send the nonce to your server.
 //                AsyncHttpClient client = new AsyncHttpClient();
 //                RequestParams params = new RequestParams();
 //                params.put("payment_method_nonce", nonce);
 //                client.post(SERVER_BASE + "/payment-methods", params,
 //					new AsyncHttpResponseHandler() {
-//					
+//
 //					    @Override
 //					    public void onSuccess(String content) {
 //		                    Toast.makeText(mContext, content, Toast.LENGTH_LONG).show();
@@ -146,20 +148,21 @@ public class MyBraintree extends CordovaPlugin {
 	        }
     	}
     }
-    
+
     private void getToken() {
-    	Log.v(TAG, "Connect to " + SERVER_BASE + "/token");
-        client.get(SERVER_BASE + "/token", new AsyncHttpResponseHandler() {
+    	//Log.v(TAG, "Connect to " + SERVER_BASE + "/token");
+
+        RequestParams requestParams = new RequestParams();
+        requestParams.put("customer_id", CUSTOMER_ID);
+        client.post(SERVER_BASE + "/token", requestParams,new AsyncHttpResponseHandler() {
 
             @Override
             public void onSuccess(String content) {
-//            	clientToken = "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiIyYzk5ODJkZDRkNDFiYjYxZmExYzUxZDczZGY2MjhjOGJmZDAxMjViNjFiNmM2MGU4NzViMWEyZTg3OWI3NTY4fGNyZWF0ZWRfYXQ9MjAxNS0xMS0wM1QxODoxODoxNy43MTA1OTQzOTcrMDAwMFx1MDAyNm1lcmNoYW50X2lkPTh4Yzh2MnZ6cDR6Nzdya2hcdTAwMjZwdWJsaWNfa2V5PTRqNzRoczJmOWQzM2RnMm0iLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvOHhjOHYydnpwNHo3N3JraC9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzh4Yzh2MnZ6cDR6Nzdya2gvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIn0sInRocmVlRFNlY3VyZUVuYWJsZWQiOmZhbHNlLCJwYXlwYWxFbmFibGVkIjp0cnVlLCJwYXlwYWwiOnsiZGlzcGxheU5hbWUiOiJWaWV3ZGFsZSBJVCIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjpudWxsLCJtZXJjaGFudEFjY291bnRJZCI6InZpZXdkYWxlaXQiLCJjdXJyZW5jeUlzb0NvZGUiOiJBVUQifSwiY29pbmJhc2VFbmFibGVkIjpmYWxzZSwibWVyY2hhbnRJZCI6Ijh4Yzh2MnZ6cDR6Nzdya2giLCJ2ZW5tbyI6Im9mZiJ9";
-
             	Bean data = gson.fromJson(content, Bean.class);
                 clientToken = data.client_token;
 
-            	Log.v(TAG, "content : " + content);
-            	Log.v(TAG, "client_token : " + clientToken);
+            	//Log.v(TAG, "content : " + content);
+            	//Log.v(TAG, "client_token : " + clientToken);
             	
 
             	if (data.success)
--- www/myBraintree.js
+++ www/myBraintree.js
@@ -1,24 +1,6 @@
 module.exports = {
 
-    Pay: function (onsuccess, onfail, base_url, amount, pri_desc, sec_desc) {
-        // Get Client Token
-        // alert("execute MyBraintree Pay");
-
-     //    $.ajax({
-     //    	url: base_url + "/token", 
-     //    	dataType: "json",
-     //    	success: function(result){
-     //    		// alert(result.client_token);
-     //    		alert(JSON.stringify(result));
-    	// 	},
-    	// 	error: function(xhr,status,error) {
-    	// 		alert("xhr" + xhr);
-    	// 		alert("status" + status);
-    	// 		alert("error" + error);
-    	// 	}
-    	// });
-
-        cordova.exec(onsuccess, onfail, "MyBraintree", "Pay", [base_url, amount, pri_desc, sec_desc]);
-
+    Pay: function (onsuccess, onfail, base_url, customer_id, amount, pri_desc, sec_desc) {
+        cordova.exec(onsuccess, onfail, "MyBraintree", "Pay", [base_url,customer_id,amount, pri_desc, sec_desc]);
     }
 }
